b471ef9c1514d830b452e1be42f77303b06acfdb
Ivan Konyushenko <idkonyushenko@gmail.com>
2025-05-21 17:44:03 +0300

    Add: abstract tester, network tester, flow visualization.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from pathlib import Path
 from PIL import Image
 import numpy as np
@@ -5,39 +6,15 @@ import cv2
 import mlflow
 from ..speed_estimator.key_points import KeyPoints
 from ..speed_estimator.neuflow import NeuFlow
+from q_camera_top.testing.flow_vis import flow_to_image
 
 
-# class NeuFlowTester():
-#     def __init__(self,
-#                  img_dir: Path):
-#         self.model: NeuFlow = NeuFlow()
-
-#         self.root = img_dir
-
-#         self.img1 = None
-#         self.img2 = None
-#         self.flow1 = None
-#         self.flow2 = None
-
-
-
-class KPTester():
+class SpeedEstimatorTester(ABC):
     def __init__(self,
-                 img_dir: Path,
-                 kp_type: str,
-                 ):
-
-        self.kp: KeyPoints = KeyPoints(debug=True,
-                                    #    roi=(0+350, 200, 1024-350, 768-200),
-                                       kp_type=kp_type)
+                 img_dir: Path):
         self.root = img_dir
-        self.kp_type = kp_type
-
         self.img1 = None
         self.img2 = None
-        self.kp1 = None
-        self.kp2 = None
-        self.matches = None
 
     def get_imgs_id(self):
         ids = []
@@ -66,6 +43,63 @@ class KPTester():
                 img2_id = file.parent / name
 
                 return img1_id, img2_id
+
+    @abstractmethod
+    def run():
+        pass
+
+
+class NeuFlowTester(SpeedEstimatorTester):
+    def __init__(self,
+                 img_dir: Path):
+        super().__init__(img_dir)
+
+        self.model: NeuFlow = NeuFlow()
+        self.flow1 = None
+        self.flow2 = None
+
+    @staticmethod
+    def _expand_flow(flow):
+        """Принимает на вход u размерность потока (горизонтальный сдвиг) и расширяет его до (u, v) потока нулями."""
+        return np.stack([flow, np.zeros_like(flow)], axis=2)
+
+    def run(self, img_id):
+        img1_path, img2_path = self._find_paths(img_id)
+
+        img1 = Image.open(img1_path)
+        img2 = Image.open(img2_path)
+        self.img1 = np.array(img1)
+        self.img2 = np.array(img2)
+
+        _ = self.model.get_momentum_speed(self.img1)
+
+        _ = self.model.get_momentum_speed(self.img1)
+        self.flow1 = self.model._prev_flow
+        self.flow1 = self._expand_flow(self.flow1)
+
+        speed = self.model.get_momentum_speed(self.img2)
+        self.flow2 = self.model._prev_flow
+        self.flow2 = self._expand_flow(self.flow2)
+
+        return speed
+
+
+
+class KPTester(SpeedEstimatorTester):
+    def __init__(self,
+                 img_dir: Path,
+                 kp_type: str,
+                 ):
+
+        super().__init__(img_dir)
+        self.kp: KeyPoints = KeyPoints(debug=True,
+                                    #    roi=(0+350, 200, 1024-350, 768-200),
+                                       kp_type=kp_type)
+        self.kp_type = kp_type
+        self.kp1 = None
+        self.kp2 = None
+        self.matches = None
+
 
     def vis_top_matches(self):
         matched_kp1_indices = [m.queryIdx for m in self.matches]
@@ -107,7 +141,7 @@ class KPTester():
 
         return img2plot
 
-    def run_kp(self, img_id: int):
+    def run(self, img_id: int):
         img1_path, img2_path = self._find_paths(img_id)
 
         img1 = Image.open(img1_path)
@@ -136,25 +170,46 @@ def log_image(img, key, step, scale=0.7):
     mlflow.log_image(img_scaled, artifact_file=key+'/'+str(step_pad)+'.png')
 
 
+def log_kp(tester, i):
+    kp = tester.vis_kp()
+    log_image(kp, key=f'key_points', step=i)
+
+    top_matches = tester.vis_top_matches()
+    log_image(top_matches, key=f'top_matches', step=i)
+
+    matches = tester.vis_matches()
+    log_image(matches, key='all_matches', step=i)
+
+def build_tester(kp_types, net_types, imgs_dir, estim_type):
+    if estim_type in kp_types:
+        tester = KPTester(imgs_dir, estim_type)
+    elif estim_type in net_types:
+        tester = NeuFlowTester(imgs_dir)
+    else:
+        raise ValueError('Incorrect speed estimator type.')
+    return tester
+
 if __name__ == '__main__':
+    kp_types = ['orb', 'sift']
+    net_types = ['neuflow']
     imgs_dir = Path('/home/devel/konyushenko/opticalFlow/framed_passes/3033_camera_top_far.mkv_20250305T093205.336800')
     # imgs_dir = Path('/home/devel/konyushenko/opticalFlow/framed_passes/1660_camera_top_far.mkv_20240424T131110.743152')
+    estim_type = 'neuflow'
 
     imgs_dir_id = imgs_dir.stem.split('_')[0]
     mlflow.set_experiment(experiment_name=imgs_dir_id)
-    for kp_type in ['orb', 'sift']:
-        with mlflow.start_run(run_name=f'{imgs_dir_id}-{kp_type}-smROI'):
-            tester = KPTester(imgs_dir, kp_type)
+    for estim_type in net_types:# + kp_types:
+        with mlflow.start_run(run_name=f'{imgs_dir_id}-{estim_type}-smROI'):
+            tester = build_tester(kp_types, net_types, imgs_dir, estim_type)
+
             img_ids = tester.get_imgs_id()
             for i, id in enumerate(img_ids[:-1]):
-                speed = tester.run_kp(id)
-                mlflow.log_metric('speed', np.abs(speed), step=i)
+                speed = tester.run(id)
+                mlflow.log_metric('speed', np.abs(np.median(speed)), step=i)
 
-                kp = tester.vis_kp()
-                log_image(kp, key=f'key_points', step=i)
+                if estim_type in kp_types:
+                    log_kp(tester, i)
 
-                top_matches = tester.vis_top_matches()
-                log_image(top_matches, key=f'top_matches', step=i)
-
-                matches = tester.vis_matches()
-                log_image(matches, key='all_matches', step=i)
+                if estim_type in net_types:
+                    log_image(flow_to_image(tester.flow2), key=f'flow', step=i)
+                    # log_image(flow_to_image(tester.flow2 - tester.flow1), key=f'flow delta', step=i)
